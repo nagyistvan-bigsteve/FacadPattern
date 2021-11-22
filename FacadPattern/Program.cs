e2145774b8cc15ed1e0efe49f7fb175ecc92e6ff
Nagy Istvan <istvan.nagy@emmanuel.ro>
2021-11-22 17:32:05 +0200

    Add project files.

---
+++ FacadPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FacadPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarFacade facade = new CarFacade();
+
+            facade.CreateCompleteCar();
+
+            Console.ReadKey();
+
+        }
+    }
+}
